1094db41894263c947fdb3c133d5e1073d9635bc
Michael Melanson <michael@michaelmelanson.net>
2020-03-31 23:19:40 -0400

    Add "C" test suite

---
+++ emulator/tests/test_rv64uc-p.rs
@@ -0,0 +1,30 @@
+mod runner;
+
+use crate::runner::run_test_suite;
+
+macro_rules! deftest {
+  ($name: ident) => {
+    paste::item! {
+      #[test]
+      fn [<rv64mi_p_ $name>] () {
+        let file = include_bytes!(concat!("riscv-tests/isa/rv64uc-p-", stringify!($name)));
+        run_test_suite(file)
+      }
+    }
+  }
+}
+
+macro_rules! deftest_ignore {
+  ($name: ident) => {
+    paste::item! {
+      #[test]
+      #[ignore]
+      fn [<rv64um_p_ $name>] () {
+        let file = include_bytes!(concat!("riscv-tests/isa/rv64uc-p-", stringify!($name)));
+        run_test_suite(file)
+      }
+    }
+  }
+}
+
+deftest_ignore!(rvc);
