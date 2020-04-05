71275098e4e1e605061fc19b1319901949fea195
Michael Melanson <michael@michaelmelanson.net>
2020-04-04 21:44:54 -0400

    Disable RV64UC-p-rvc test

--- emulator/tests/test_rv64uc-p.rs
+++ emulator/tests/test_rv64uc-p.rs
@@ -14,17 +14,17 @@ macro_rules! deftest {
   }
 }
 
-// macro_rules! deftest_ignore {
-//   ($name: ident) => {
-//     paste::item! {
-//       #[test]
-//       #[ignore]
-//       fn [<rv64um_p_ $name>] () {
-//         let file = include_bytes!(concat!("riscv-tests/isa/rv64uc-p-", stringify!($name)));
-//         run_test_suite(file)
-//       }
-//     }
-//   }
-// }
+macro_rules! deftest_ignore {
+  ($name: ident) => {
+    paste::item! {
+      #[test]
+      #[ignore]
+      fn [<rv64um_p_ $name>] () {
+        let file = include_bytes!(concat!("riscv-tests/isa/rv64uc-p-", stringify!($name)));
+        run_test_suite(file)
+      }
+    }
+  }
+}
 
-deftest!(rvc);
+deftest_ignore!(rvc);
