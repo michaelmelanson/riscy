27be7a1fe59b0622c051050c0fd35fa38bec7ed7
Michael Melanson <michael@michaelmelanson.net>
2020-04-10 20:48:55 -0400

    Rewrite the instruction decoding

--- isa/src/lib.rs
+++ isa/src/lib.rs
@@ -872,10 +872,15 @@ impl Instruction {
 
     match opcode {
       Opcode::CADDI4SPN => {
-        let nzuimm = (encoded >> 5) & 0b11111111;
+        let nzuimm_5_4 = ((encoded >> 11) & 0b11) << 4;
+        let nzuimm_9_6 = ((encoded >> 7) & 0b1111) << 6;
+        let nzuimm_2 = ((encoded >> 6) & 0b1) << 2;
+        let nzuimm_3 = ((encoded >> 5) & 0b1) << 3;
+        let nzuimm = nzuimm_5_4 | nzuimm_9_6 | nzuimm_2 | nzuimm_3;
+
         let rd = ((encoded >> 2) & 0b111) as u8;
         
-        let imm = nzuimm * 4;
+        let imm = nzuimm;
         let rd = Register::from_u8(rd + 8);
 
         Instruction::CIW { opcode, imm, rd }
@@ -923,12 +928,15 @@ impl Instruction {
       },
 
       Opcode::CADDI16SP => {
-        let nzuimm_9 = (encoded >> 12) & 0b1;
-        let nzuimm_etc = (encoded >> 2) & 0b11111;
-        let sign_bit = nzuimm_9;
-        let imm = ((if sign_bit > 0 { 0b1111111111111111 << 10 } else { 0 })
-                     | (nzuimm_etc << 4) 
-                     | (nzuimm_9 << 9)) as i16 as i64;
+        let nzuimm_9 = ((encoded >> 12) & 0b1) << 9;
+        let nzuimm_4 = ((encoded >> 6) & 0b1) << 4;
+        let nzuimm_6 = ((encoded >> 5) & 0b1) << 6;
+        let nzuimm_8_7 = ((encoded >> 3) & 0b11) << 7;
+        let nzuimm_5 = ((encoded >> 2) & 0b1) << 5;
+
+        let sign_bit = nzuimm_9 != 0;
+        let imm = ((if sign_bit { 0b1111111111111111 << 10 } else { 0 })
+                     | nzuimm_9 | nzuimm_4 | nzuimm_6 | nzuimm_8_7 | nzuimm_5) as i16 as i64;
                 
         let rd = Register::StackPointer;
 
@@ -1011,23 +1019,38 @@ impl Instruction {
         Instruction::CI { opcode, imm, rd }
       },
 
-      Opcode::CSRLI | Opcode::CSRAI | Opcode::CANDI | Opcode::CBEQZ | Opcode::CBNEZ => {
-        let offset_8 = ((encoded >> 12) & 0b1) << 8;
-        let offset_4_3 = ((encoded >> 10) & 0b11) << 3;
-        let offset_7_6 = ((encoded >> 5) & 0b11) << 6;
-        let offset_2_1 = ((encoded >> 3) & 0b11) << 1;
-        let offset_5 = ((encoded >> 2) & 0b1) << 5;
+      Opcode::CSRLI | Opcode::CSRAI | Opcode::CANDI => {
+        let shamt_5 = ((encoded >> 12) & 0b1) << 5;
+        let shamt_4_0 = (encoded >> 2) & 0b11111;
         
-        let sign_bit = offset_8;
+        let sign_bit = shamt_5 != 0;
         let imm = 
-          (if sign_bit > 0 { (-1i16 as u16) << 9 } else { 0 }) |
-          offset_8 | offset_4_3 | offset_7_6 | offset_2_1 | offset_5;
+          (if sign_bit { (-1i16 as u16) << 6 } else { 0 }) |
+          shamt_5 | shamt_4_0;
         let imm = imm as i16;
         
         let rs1 = Register::from_rd_prime(((encoded >> 7) & 0b111) as u8);
 
         Instruction::CB { opcode, imm, rs1 }
       },
+
+      Opcode::CBEQZ | Opcode::CBNEZ => {
+        let offset_8 = ((encoded >> 12) & 0b1) << 8;
+        let offset_4_3 = ((encoded >> 10) & 0b11) << 3;
+        let offset_7_6 = ((encoded >> 5) & 0b11) << 6;
+        let offset_2_1 = ((encoded >> 3) & 0b11) << 1;
+        let offset_5 = ((encoded >> 2) & 0b1) << 5;
+        
+        let sign_bit = offset_8 != 0;
+        let imm = 
+          (if sign_bit { (-1i16 as u16) << 9 } else { 0 }) |
+          offset_8 | offset_4_3 | offset_7_6 | offset_2_1 | offset_5;
+        let imm = imm as i16;
+        
+        let rs1 = Register::from_rd_prime(((encoded >> 7) & 0b111) as u8);
+
+        Instruction::CB { opcode, imm, rs1 }
+      }
 
       Opcode::CAND | Opcode::COR | Opcode::CXOR | Opcode::CSUB | Opcode::CADDW | Opcode::CSUBW => {
         let rd = Register::from_rd_prime(((encoded >> 7) & 0b111) as u8);
@@ -1060,9 +1083,20 @@ impl Instruction {
         Instruction::CR { opcode, rd, rs2 }
       },
 
-      Opcode::CSWSP | Opcode::CSDSP => {
+      Opcode::CSWSP => {
         let rs2 = Register::from_u8(((encoded >> 2) & 0b11111) as u8);
-        let imm = (encoded >> 7) & 0b111111;
+        let offset_5_2 = ((encoded >> 9) & 0b1111) << 2;
+        let offset_7_6 = ((encoded >> 7) & 0b11) << 6;
+        let imm = offset_5_2 | offset_7_6;
+
+        Instruction::CSS { opcode, imm, rs2 }
+      },
+
+      Opcode::CSDSP => {
+        let rs2 = Register::from_u8(((encoded >> 2) & 0b11111) as u8);
+        let offset_5_3 = ((encoded >> 10) & 0b111) << 3;
+        let offset_8_6 = ((encoded >> 7) & 0b111) << 6;
+        let imm = offset_5_3 | offset_8_6;
 
         Instruction::CSS { opcode, imm, rs2 }
       },
@@ -1074,20 +1108,8 @@ impl Instruction {
   pub fn from_32bits(encoded: u32) -> Instruction {
     let funct3   = (encoded >> 12) & 0b111;
     let funct7   = (encoded >> 25) & 0b1111111;
-    let rd       = (encoded >> 7) & 0b11111;
-    let rs1      = (encoded >> 15) & 0b11111;
-    let rs2      = (encoded >> 20) & 0b11111;
-    
-    let imm31_12  = encoded >> 12;
-    let imm19_12  = (encoded >> 12) & 0b11111111;
-    let imm12     = (encoded >> 31) & 0b1;
-    let imm11_5   = (encoded >> 25) & 0b1111111;
-    let imm10_5   = (encoded >> 25) & 0b111111;
-    let imm4_1    = (encoded >> 8) & 0b1111;
     let imm11_0   = (encoded >> 20) & 0b111111111111;
-    let imm4_0    = (encoded >> 7) & 0b11111;
 
-    let sign_bit = (encoded >> 31) & 0b1;
     let opcode = Opcode::new(
       encoded as u16, 
       funct3 as u8,
@@ -1095,107 +1117,122 @@ impl Instruction {
       funct7 as u8
     );
 
-    let from_r_type = |opcode| {
-      Instruction::R { 
-        opcode,
-        rs2: Register::from_u8(rs2 as u8),
-        rs1: Register::from_u8(rs1 as u8),
-        rd: Register::from_u8(rd as u8)
-      }
-    };
-
-    let from_i_type = |opcode| {
-      let imm: u32 = (if sign_bit > 0 { 0b11111111111111111111 << 12 } else { 0 })
-                   | imm11_0;
-
-      Instruction::I { 
-        opcode, 
-        rd: Register::from_u8(rd as u8), 
-        imm: imm as i32, 
-        rs1: Register::from_u8(rs1 as u8) 
-      }
-    };
-
-    let from_is_type = |opcode| {
-      let imm: u32 = imm11_0;
-
-      Instruction::IS { 
-        opcode, 
-        rd: Register::from_u8(rd as u8), 
-        imm: imm as u32, 
-        rs1: Register::from_u8(rs1 as u8) 
-      }
-    };
-
-    let from_s_type = |opcode| {
-      let imm = (if sign_bit > 0 { 0b11111111111111111111 << 12 } else { 0 }) 
-                   | (imm11_5 << 5) 
-                   | imm4_0;
-
-      Instruction::S { 
-        opcode, 
-        rs2: Register::from_u8(rs2 as u8),
-        rs1: Register::from_u8(rs1 as u8), 
-        imm: imm as i32
-      }
-    };
-
-    let from_b_type = |opcode| {
-      let imm11 = (encoded >> 7) & 0b1;
-      let imm = (if sign_bit > 0 { 0b11111111111111111111 << 12 } else { 0 })
-                   | (imm12 << 12)
-                   | (imm11 << 11)
-                   | (imm10_5 << 5)
-                   | (imm4_1 << 1);
-
-      Instruction::B {
-        opcode, 
-        rs1: Register::from_u8(rs1 as u8),
-        rs2: Register::from_u8(rs2 as u8), 
-        imm: imm as i32
-      }
-    };
-
-    let from_u_type = |opcode| {
-      let imm = imm31_12 << 12;
-
-      Instruction::U {
-        opcode, 
-        rd: Register::from_u8(rd as u8), 
-        imm: imm as i32 
-      }
-    };
-
-    let from_j_type = |opcode| {
-      let imm11 = (encoded >> 20) & 0b1;
-      let imm10_1   = (encoded >> 21) & 0b1111111111;
-
-      let imm = (if sign_bit > 0 { 0b111111111111 << 20 } else { 0 })
-                   | (imm19_12 << 12)
-                   | (imm11 << 11)
-                   | (imm10_1 << 1);
-
-      Instruction::J {
-        opcode,
-        rd: Register::from_u8(rd as u8),
-        imm: imm as i32
-      }
-    };
-
     let instruction = match opcode {
-      Opcode::Lui       => from_u_type(opcode),
-      Opcode::AuiPc     => from_u_type(opcode),
-      Opcode::JAl       => from_j_type(opcode),
-      Opcode::JAlr      => from_i_type(opcode),
-      Opcode::Branch(_) => from_b_type(opcode),
-      Opcode::Load(_)   => from_i_type(opcode),
-      Opcode::Store(_)  => from_s_type(opcode),
-      Opcode::OpImm(_)  => from_i_type(opcode),
-      Opcode::OpImm32(_) => from_i_type(opcode),
-      Opcode::Op(_)     => from_r_type(opcode),
-      Opcode::Op32(_)   => from_r_type(opcode),
-      Opcode::System(_) => from_is_type(opcode),
-      Opcode::MiscMem(_) => from_i_type(opcode),
+      Opcode::Lui | Opcode::AuiPc => {
+        let imm = ((encoded >> 12) & 0b11111111111111111111) << 12;
+        let rd = (encoded >> 7) & 0b11111;
+  
+        Instruction::U {
+          opcode, 
+          rd: Register::from_u8(rd as u8), 
+          imm: imm as i32 
+        }
+      },
+
+      Opcode::JAl => {
+        let imm20 = ((encoded >> 31) & 0b1) << 20;
+        let imm10_1 = ((encoded >> 21) & 0b11111111111) << 1;
+        let imm11 = ((encoded >> 20) & 0b1) << 11;
+        let imm19_12 = ((encoded >> 12) & 0b11111111) << 12;
+
+        let sign_bit = imm20 != 0;
+        let imm = (if sign_bit { (-1i32 as u32) << 21 } else { 0 })
+                     | imm20 | imm10_1 | imm11 | imm19_12;
+  
+        let rd = (encoded >> 7) & 0b11111;
+        Instruction::J {
+          opcode,
+          rd: Register::from_u8(rd as u8),
+          imm: imm as i32
+        }
+      },
+
+      Opcode::JAlr | Opcode::Load(_) | Opcode::OpImm(_) | Opcode::OpImm32(_) | Opcode::MiscMem(_) => {
+        let imm11 = ((encoded >> 31) & 0b1) << 11;
+        let imm10_0 = (encoded >> 20) & 0b111111111111;
+        let sign_bit = imm11 != 0;
+
+        let imm: u32 = (if sign_bit { (-1i32 as u32) << 12 } else { 0 })
+                     | imm11 | imm10_0;
+  
+        let rd = (encoded >> 7) & 0b11111;
+        let rs1 = (encoded >> 15) & 0b11111;
+
+        Instruction::I { 
+          opcode, 
+          rd: Register::from_u8(rd as u8), 
+          imm: imm as i32, 
+          rs1: Register::from_u8(rs1 as u8) 
+        }
+      },
+
+      Opcode::Branch(_) => {
+        let imm12 = ((encoded >> 31) & 0b1) << 12;
+        let imm10_5 = ((encoded >> 25) & 0b111111) << 5;
+        let imm4_1 = ((encoded >> 8) & 0b1111) << 1;
+        let imm11 = ((encoded >> 7) & 0b1) << 11;
+
+        let sign_bit = imm12 != 0;
+
+        let imm = (if sign_bit { (-1i32 as u32) << 13 } else { 0 })
+                     | imm12 | imm10_5 | imm4_1 | imm11;
+
+        let rs1 = (encoded >> 15) & 0b11111;
+        let rs2 = (encoded >> 20) & 0b11111;
+  
+        Instruction::B {
+          opcode, 
+          rs1: Register::from_u8(rs1 as u8),
+          rs2: Register::from_u8(rs2 as u8), 
+          imm: imm as i32
+        }
+      },
+
+      Opcode::Store(_) => {
+        let imm11 = (encoded >> 31) << 11;
+        let imm10_5 = ((encoded >> 25) & 0b111111) << 5;
+        let imm4_0 = (encoded >> 7) & 0b11111;
+        let sign_bit = imm11 != 0;
+
+        let imm = (if sign_bit { (-1i32 as u32) << 12 } else { 0 }) 
+                     | imm11 | imm10_5 | imm4_0;
+  
+        let rs2 = (encoded >> 20) & 0b11111;
+        let rs1 = (encoded >> 15) & 0b11111;
+
+        Instruction::S { 
+          opcode, 
+          rs2: Register::from_u8(rs2 as u8),
+          rs1: Register::from_u8(rs1 as u8), 
+          imm: imm as i32
+        }
+      },
+
+      Opcode::Op(_) | Opcode::Op32(_) => {
+        let rs2 = (encoded >> 20) & 0b11111;
+        let rs1 = (encoded >> 15) & 0b11111;
+        let rd  = (encoded >> 7)  & 0b11111;
+
+        Instruction::R {
+          opcode,
+          rs2: Register::from_u8(rs2 as u8),
+          rs1: Register::from_u8(rs1 as u8),
+          rd: Register::from_u8(rd as u8)
+        }
+      },
+
+      Opcode::System(_) => {
+        let imm = (encoded >> 20) & 0b111111111111;
+        let rs1 = (encoded >> 15) & 0b11111;
+        let rd  = (encoded >> 7) & 0b11111;
+
+        Instruction::IS { 
+          opcode, 
+          rd: Register::from_u8(rd as u8), 
+          imm: imm as u32, 
+          rs1: Register::from_u8(rs1 as u8) 
+        }
+      },
 
       _ => unimplemented!("opcode {:?}", opcode)
     };
