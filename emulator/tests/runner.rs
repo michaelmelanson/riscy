e81fa2638b941ede4e8823b054fb5678963dd984
Michael Melanson <michael@michaelmelanson.net>
2020-05-24 21:18:20 -0400

    Fix up memory error in test runner

--- emulator/tests/runner.rs
+++ emulator/tests/runner.rs
@@ -1,7 +1,7 @@
 use riscy_emulator::{
   subsystem::{Subsystem, SubsystemAction}, 
   machine::{RiscvMachine, RiscvMachineError}, 
-  memory::{Region, Memory}
+  memory::{Region, Memory, Permissions}
 };
 use riscy_isa::{Register};
 
@@ -38,18 +38,22 @@ pub fn run_test_suite(file: &[u8]) {
 
   let binary = goblin::elf::Elf::parse(file).expect("Could not parse file");
   
-  let mut memory = Memory::new();
-  memory.add_region(Region::readwrite_memory(0, 1024*1024*1024*4));
-
+  let mut region = Region::readwrite_memory(0, 1024*1024*1024*4);
+  
   for ph in binary.program_headers {
     // println!("Loading header: {:?}", ph);
     let header_bytes = &file[ph.file_range()];
-
+    
     for (offset, byte) in header_bytes.iter().enumerate() {
       let address = (ph.p_vaddr as usize) + (offset as usize);
-      memory.store_byte(address as u64, *byte as u64).expect("write to memory");
+      region.write(address as u64, *byte).expect("write to memory");
     }
   }
+  
+  let region = region.change_permissions(Permissions::custom(true, true));
+
+  let mut memory = Memory::new();
+  memory.add_region(region);
 
   let mut machine = RiscvMachine::<TestRunnerSubsystem>::new(memory, binary.header.e_entry);
   machine.state_mut().registers.set(Register::StackPointer, 0x1000);
