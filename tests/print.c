9e185705a867fc18c9cd99f9355a7fc1f632a92e
Michael Melanson <michael@michaelmelanson.net>
2021-08-07 19:18:13 -0400

    Change print.c to selfie-compatible C*

--- tests/print.c
+++ tests/print.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+void print(uint64_t* s) {
+  uint64_t* buffer;
+  buffer = s;
+
+  while (*buffer != 0) {
+    write(1, buffer, 8);
+    buffer = buffer + 1;
+  }
+}
 
 int main() {
-  puts("Hello world!");
-  return 0;
+  print("Hello world!");
+  return 55;
 }
